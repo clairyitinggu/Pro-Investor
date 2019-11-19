548be9091d4f71003871b5ef9c8172e84f9bbb53
unknown <madelinefuen@umass.edu>
2019-11-19 13:59:16 -0500

    home page with delete

--- public/js/api_helper.js
+++ public/js/api_helper.js
@@ -2,6 +2,8 @@ const express = require('express')
 var path = require("path");
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+const alpha = require('alphavantage')({ key: 'Z3B2FO26F96IVKYH' });
+var io = require('socket.io')
 
 const app = express()
 
@@ -9,13 +11,32 @@ app.use(express.static(path.join(__dirname, '../../public')));
 app.set('view engine', 'ejs')
 
 
-list = ['one', 'two', 'three']
-
-//display login page
+//display home page
 app.get('/', function (request, response) {
-  response.render('api_helper', {result: list})
+  response.render('home', {result: list})
 })
 
-app.listen(3000, function () {
+server = app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+
+var listener = io.listen(server);
+
+list = []
+stocks = ['msft', 'amzn']
+
+for(symbol of stocks){
+  alpha.data.quote(symbol).then(data => {
+    list.push(data)
+  });
+}
+
+listener.on('connection', function (socket) { 
+  socket.emit('initialize', list); // Emit on the opened socket.
+});
+
+alpha.data.search('microsoft').then(data => {
+  console.log(data)
+});
+
