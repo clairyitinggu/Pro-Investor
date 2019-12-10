01c5fc35e380bf0b2bf596fa2c4ea06eba3fddeb
jiaju <jiaju@DESKTOP-LJAIAAH>
2019-12-09 22:05:02 -0500

    this should be good now, all files up to date

--- public/js/api.js
+++ public/js/api.js
@@ -2,9 +2,10 @@ const express = require('express')
 var path = require("path");
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-const alpha = require('alphavantage')({ key: 'M9XXXH6V2NGIA32C' }); //Z3B2FO26F96IVKYH  //M9XXXH6V2NGIA32C
+const alpha = require('alphavantage')({ key: 'M9XXXH6V2NGIA32C' }); 
 const fetch = require("node-fetch"); //npm i node-fetch --save
-
+var keyCounter = 0;
+var keyArray = ['M9XXXH6V2NGIA32C', 'Z3B2FO26F96IVKYH', 'KR9H9KUW7FR4EWAQ','ICB7Q3R5EMA32GYY']
 module.exports = {
     getApiData: async function(list,symbols) {
         const otherPram = {
@@ -14,9 +15,14 @@ module.exports = {
             method: "GET"
         };
         for (itr of symbols) {
-            url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=" + alpha.key + "&symbol=" + itr;
-            response = await fetch(url, otherPram);
-            data = await response.json();
+            var temp;
+            var data;
+            do {
+                keyCounter = (keyCounter + 1) % keyArray.length;
+                url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=" + keyArray[keyCounter] + "&symbol=" + itr;
+                response = await fetch(url, otherPram);
+                data = await response.json();
+            } while (JSON.stringify(data).includes('5 calls per minute'));
             await list.push(data);
         }
         return list;
